5b9ca5a56e994ac112440450b82775b11c3f4bdb
Contempl <the_mega_gamexc76@mail.ru>
2025-12-08 03:06:42 +0300

    Typo mistake refactored

--- DirectoryService/src/DirectoryService.Domain/Entities/Location.cs
+++ DirectoryService/src/DirectoryService.Domain/Entities/Location.cs
@@ -65,8 +65,8 @@ public record Timezone
 
     public static Result<Timezone> Create(string windowsId, string? region)
     {
-        var convertionResult = TimeZoneInfo.TryConvertWindowsIdToIanaId(windowsId, region, out var ianaId);
-        if (!convertionResult is false || ianaId is null)
+        var conversionResult = TimeZoneInfo.TryConvertWindowsIdToIanaId(windowsId, region, out var ianaId);
+        if (!conversionResult is false || ianaId is null)
             return Result.Failure<Timezone>("Can't convert windows id to IanaId. Please try again.");
 
         return new Timezone(ianaId);
