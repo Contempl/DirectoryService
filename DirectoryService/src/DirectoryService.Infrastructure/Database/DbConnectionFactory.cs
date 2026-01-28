4574dfb74de57e1e406e532fcccfbdf103faaa8a
Contempl <the_mega_gamexc76@mail.ru>
2026-01-28 14:57:04 +0300

    Создание фабрики подключения для Dapper

---
+++ DirectoryService/src/DirectoryService.Infrastructure/Database/DbConnectionFactory.cs
@@ -0,0 +1,36 @@
+using System.Data;
+using DirectoryService.Application.Database;
+using Microsoft.Extensions.Configuration;
+using Npgsql;
+using Microsoft.Extensions.Logging;
+
+namespace DirectoryService.Infrastructure.Database;
+
+public class NpgsqlConnectionFactory : IDbConnectionFactory, IDisposable, IAsyncDisposable
+{
+    private readonly NpgsqlDataSource _dataSource;
+
+    public NpgsqlConnectionFactory(IConfiguration configuration)
+    {
+        var dataSourceBuilder = new NpgsqlDataSourceBuilder(configuration.GetConnectionString("Database"));
+        dataSourceBuilder.UseLoggerFactory(CreateLoggerFactory());
+        _dataSource = dataSourceBuilder.Build();
+    }
+    public async Task<IDbConnection> CreateConnectionAsync(CancellationToken cancellationToken = default)
+    {
+        return await _dataSource.OpenConnectionAsync(cancellationToken);
+    }
+    
+    private ILoggerFactory CreateLoggerFactory() =>
+        LoggerFactory.Create(builder => { builder.AddConsole(); });
+
+    public void Dispose()
+    {
+        _dataSource.Dispose();
+    }
+
+    public async ValueTask DisposeAsync()
+    {
+        await _dataSource.DisposeAsync();
+    }
+}
