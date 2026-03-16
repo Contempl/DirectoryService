b5ed4b07a4058803a68ecbb2ea63b4d67472af8c
contempl <the_mega_gamexc76@mail.ru>
2026-03-16 19:42:53 +0300

    Implemented shared service

---
+++ Shared/Core/Abstractions/ICommandHandler.cs
@@ -0,0 +1,18 @@
+using CSharpFunctionalExtensions;
+using Shared.Kernel;
+
+namespace Core.Abstractions;
+
+public interface ICommand;
+
+public interface ICommandHandler<TResponse, in TCommand>
+    where TCommand : ICommand
+{
+    Task<Result<TResponse, Errors>> HandleAsync(TCommand command, CancellationToken cancellationToken);
+}
+
+public interface ICommandHandler<in TCommand>
+    where TCommand : ICommand
+{
+    Task<UnitResult<Errors>> Handle(TCommand command, CancellationToken cancellationToken);
+}
---
+++ Shared/Framework/Middleware/ExceptionMiddleware.cs
@@ -0,0 +1,49 @@
+using Framework.Response;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Shared.Kernel;
+
+namespace Framework.Middleware;
+
+public class ExceptionMiddleware
+{
+    private readonly RequestDelegate _next;
+    private readonly ILogger<ExceptionMiddleware> _logger;
+
+    public ExceptionMiddleware(RequestDelegate next, ILogger<ExceptionMiddleware> logger)
+    {
+        _next = next;
+        _logger = logger;
+    }
+
+    public async Task InvokeAsync(HttpContext httpContext)
+    {
+        try
+        {
+             await _next(httpContext);   
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, ex.Message);
+
+            var error = Error.Failure("server.internal.error", ex.Message);
+
+            var envelope = Envelope.Error(error);
+
+            httpContext.Response.ContentType = "application/json";
+
+            httpContext.Response.StatusCode = StatusCodes.Status500InternalServerError;
+
+            await httpContext.Response.WriteAsJsonAsync(envelope);
+        }
+    }
+}
+
+public static class ExceptionMiddlewareExtensions
+{
+    public static IApplicationBuilder UseExceptionHandlingMiddleware(this IApplicationBuilder applicationBuilder)
+    {
+        return applicationBuilder.UseMiddleware<ExceptionMiddleware>();
+    }
+}
