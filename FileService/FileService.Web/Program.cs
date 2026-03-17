6648e065b6bd7a74e653559df1d58cd848d7bbdb
contempl <the_mega_gamexc76@mail.ru>
2026-03-17 20:26:11 +0300

    Создал и настроил File Service

---
+++ FileService/FileService.Web/Program.cs
@@ -0,0 +1,30 @@
+using Framework.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddCors(); 
+
+var app = builder.Build();
+
+app.MapOpenApi();
+
+
+app.UseExceptionHandlingMiddleware();
+
+app.UseCors(bld =>
+{
+    bld.WithOrigins("http://localhost:3000")
+        .AllowCredentials()
+        .AllowAnyHeader()
+        .AllowAnyMethod();
+});
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
+app.UseHttpsRedirection();
+
+app.Run();
