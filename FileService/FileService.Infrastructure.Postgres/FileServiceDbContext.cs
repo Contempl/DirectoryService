a004b2f3159a53d3e4f3ed792c14bff5e16486a2
Contempl <the_mega_gamexc76@mail.ru>
2026-03-23 01:47:03 +0300

    Необходимое для создания базы данных

--- FileService/FileService.Infrastructure.Postgres/FileServiceDbContext.cs
+++ FileService/FileService.Infrastructure.Postgres/FileServiceDbContext.cs
@@ -1,11 +1,23 @@
-﻿using FileService.Domain.Assets;
+﻿using FileService.Core.Database;
+using FileService.Domain.Assets;
+using FileService.Domain.ValueObjects;
+using FileService.Infrastructure.Postgres.Configurations;
 using Microsoft.EntityFrameworkCore;
 
 namespace FileService.Infrastructure.Postgres;
 
-public class FileServiceDbContext : DbContext
+public class FileServiceDbContext : DbContext, IReadDbContext
 {
+    public FileServiceDbContext(DbContextOptions<FileServiceDbContext> options) 
+        : base(options) { }
     public DbSet<MediaAsset> MediaAssets => Set<MediaAsset>();
 
     public IQueryable<MediaAsset> MediaAssetsQuery => MediaAssets.AsQueryable().AsNoTracking();
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Ignore<StorageKey>(); // без этого миграция не накатывается из-за того что мы ручками настроили сериализацию в конфигурации
+        
+        modelBuilder.ApplyConfiguration(new MediaAssetConfiguration());
+    }
 }
