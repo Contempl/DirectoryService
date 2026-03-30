669f970028dcb6144f1cf750a508fae8a2e484c8
contempl <the_mega_gamexc76@mail.ru>
2026-03-30 18:09:05 +0300

    Ошибка Conflict для дубликатов email

---
+++ AuthService/AuthService.Domain/Shared/AuthErrors.cs
@@ -0,0 +1,9 @@
+﻿using Shared.Kernel;
+
+namespace AuthService.Domain.Shared;
+
+public static class AuthErrors
+{
+    public static Error EmailAlreadyTaken() => 
+        Error.Conflict("auth.email_taken", "Email is already taken");
+}
