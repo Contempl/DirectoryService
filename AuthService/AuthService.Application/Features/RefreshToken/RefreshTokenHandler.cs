0eec4c53872e033e543bac00a7ae7d42721ab235
contempl <the_mega_gamexc76@mail.ru>
2026-03-31 16:03:21 +0300

    Добавил транзакции и обернул хендер в try/catch

--- AuthService/AuthService.Application/Features/RefreshToken/RefreshTokenHandler.cs
+++ AuthService/AuthService.Application/Features/RefreshToken/RefreshTokenHandler.cs
@@ -1,4 +1,5 @@
-﻿using AuthService.Domain.Entities;
+﻿using AuthService.Application.Database;
+using AuthService.Domain.Entities;
 using Core.Abstractions;
 using CSharpFunctionalExtensions;
 using Microsoft.AspNetCore.Identity;
@@ -9,6 +10,7 @@ namespace AuthService.Application.Features.RefreshToken;
 
 public class RefreshTokenHandler : ICommandHandler<Domain.Entities.RefreshToken, RefreshTokenRequest>
 {
+    private readonly ITransactionManager _transactionManager;
     private readonly IRefreshTokensRepository _refreshTokensRepository;
     private readonly ITokenProvider _tokenProvider;
     private readonly UserManager<ApplicationUser> _userManager;
@@ -17,54 +19,85 @@ public class RefreshTokenHandler : ICommandHandler<Domain.Entities.RefreshToken,
     public RefreshTokenHandler(
         IRefreshTokensRepository refreshTokensRepository,
         ILogger<RefreshTokenHandler> logger, 
-        UserManager<ApplicationUser> userManager, ITokenProvider tokenProvider)
+        UserManager<ApplicationUser> userManager,
+        ITokenProvider tokenProvider,
+        ITransactionManager transactionManager)
     {
         _refreshTokensRepository = refreshTokensRepository;
         _userManager = userManager;
         _tokenProvider = tokenProvider;
+        _transactionManager = transactionManager;
         _logger = logger;
     }
 
     public async Task<Result<Domain.Entities.RefreshToken, Errors>> HandleAsync(RefreshTokenRequest request, CancellationToken cancellationToken)
     {
-        var fetchToken = await _refreshTokensRepository
-            .GetByTokenAsync(request.RefreshToken, cancellationToken);
-        
-        if (fetchToken.IsFailure)
-        {
-            _logger.LogInformation("Could not fetch refresh token");
-            return fetchToken.Error.ToErrors();
-        }
-        
-        var refreshToken = fetchToken.Value;
-        
-        var user = await _userManager.FindByIdAsync(refreshToken.UserId.ToString());
-        if (user is null)
-        {
-            _logger.LogInformation("Could not find user with id {UserId}", refreshToken.UserId.ToString());
-            return GeneralErrors.NotFound(name: nameof(ApplicationUser)).ToErrors();
-        }
 
-        if (refreshToken.IsRevoked)
+        var beginTransactionAsync = await _transactionManager.BeginTransactionAsync(cancellationToken);
+        if (beginTransactionAsync.IsFailure)
         {
-            _logger.LogInformation("Refresh token was revoked. Removing all refresh tokens for this user.");
-            await _refreshTokensRepository.RevokeAllRefreshTokensFromUser(user.Id);
+            _logger.LogInformation("Failed to begin transaction,");
             return GeneralErrors.Failure().ToErrors();
         }
-        
-        if (refreshToken.ExpiryDate < DateTime.UtcNow)
-            return GeneralErrors.ValueIsInvalid(nameof(RefreshToken)).ToErrors();
-        
-        var newRefToken = _tokenProvider.GenerateRefreshToken(user.Id, refreshToken.Id);
-        if (newRefToken.IsFailure)
-            return newRefToken.Error.ToErrors();
 
-        var newRefreshToken = newRefToken.Value;
-        
-        refreshToken.Revoke(newRefreshToken.Token);
-        
-        await _refreshTokensRepository.AddAsync(newRefreshToken, cancellationToken);
+        using var transactionScope = beginTransactionAsync.Value;
 
-        return newRefreshToken;
+        try
+        {
+            var fetchToken = await _refreshTokensRepository
+                .GetByTokenAsync(request.RefreshToken, cancellationToken);
+        
+            if (fetchToken.IsFailure)
+            {
+                _logger.LogInformation("Could not fetch refresh token");
+                return fetchToken.Error.ToErrors();
+            }
+        
+            var refreshToken = fetchToken.Value;
+        
+            var user = await _userManager.FindByIdAsync(refreshToken.UserId.ToString());
+            if (user is null)
+            {
+                _logger.LogInformation("Could not find user with id {UserId}", refreshToken.UserId.ToString());
+                return GeneralErrors.NotFound(name: nameof(ApplicationUser)).ToErrors();
+            }
+
+            if (refreshToken.IsRevoked)
+            {
+                _logger.LogInformation("Refresh token was revoked. Removing all refresh tokens for this user.");
+                await _refreshTokensRepository.RevokeAllRefreshTokensFromUser(user.Id);
+                return GeneralErrors.Failure().ToErrors();
+            }
+        
+            if (refreshToken.ExpiryDate < DateTime.UtcNow)
+                return GeneralErrors.ValueIsInvalid(nameof(RefreshToken)).ToErrors();
+        
+            var newRefToken = _tokenProvider.GenerateRefreshToken(user.Id, refreshToken.Id);
+            if (newRefToken.IsFailure)
+                return newRefToken.Error.ToErrors();
+
+            var newRefreshToken = newRefToken.Value;
+        
+            refreshToken.Revoke(newRefreshToken.Token);
+        
+            await _refreshTokensRepository.AddAsync(newRefreshToken, cancellationToken);
+
+            var commitResult = transactionScope.Commit();
+            if (commitResult.IsFailure)
+            {
+                transactionScope.Rollback();
+                _logger.LogInformation("Failed to commit  transaction.");
+            
+                return commitResult.Error.ToErrors();
+            }
+        
+            return newRefreshToken;
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError("Error occured during transaction: {ex}", ex);
+            transactionScope.Rollback();
+            return GeneralErrors.Failure().ToErrors();
+        }
     }
 }
---
+++ AuthService/AuthService.Infrastructure/Database/TransactionManager.cs
@@ -0,0 +1,54 @@
+using AuthService.Application.Database;
+using CSharpFunctionalExtensions;
+using Microsoft.EntityFrameworkCore.Storage;
+using Microsoft.Extensions.Logging;
+using Shared.Kernel;
+
+namespace AuthService.Core.Database;
+
+public class TransactionManager : ITransactionManager
+{
+    private readonly AuthDbContext _dbContext;
+    private readonly ILogger<TransactionScope> _logger;
+    private readonly ILoggerFactory _loggerFactory;
+
+    public TransactionManager(AuthDbContext dbContext, ILogger<TransactionScope> logger, ILoggerFactory loggerFactory)
+    {
+        _dbContext = dbContext;
+        _logger = logger;
+        _loggerFactory = loggerFactory;
+    }
+
+    public async Task<Result<ITransactionScope, Error>> BeginTransactionAsync(CancellationToken cancellationToken = default)
+    {
+        try
+        {
+            var transaction = await _dbContext.Database.BeginTransactionAsync(cancellationToken);
+            
+            var transactionScopeLogger = _loggerFactory.CreateLogger<TransactionScope>();
+            
+            var transactionScope = new TransactionScope(transaction.GetDbTransaction(), transactionScopeLogger);
+
+            return transactionScope;
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e, "Failed to begin transaction");
+            return Error.Failure("database", "Failed to begin transaction");
+        }
+    }
+
+    public async Task<UnitResult<Error>> SaveChangesAsync(CancellationToken cancellationToken)
+    {
+        try
+        {
+            await _dbContext.SaveChangesAsync(cancellationToken);
+            return UnitResult.Success<Error>();
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Failed to save changes");
+            return Error.Failure("database", "Failed to save changes");
+        }
+    }
+}
