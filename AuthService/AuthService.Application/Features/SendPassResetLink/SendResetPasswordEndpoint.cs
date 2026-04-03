1f1d23bd34e9cc17d0afcfe1c818f5ec73ce4aa5
Contempl <the_mega_gamexc76@mail.ru>
2026-04-04 00:51:26 +0300

    Отправление ссылки не ресет пароля по почте

---
+++ AuthService/AuthService.Application/Features/SendPassResetLink/SendResetPasswordEndpoint.cs
@@ -0,0 +1,21 @@
+using AuthService.Contracts.Result;
+using Framework.Response;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+
+namespace AuthService.Application.Features.SendPassResetLink;
+
+public class SendResetPasswordEndpoint : IEndpoint
+{
+    public void MapEndpoint(IEndpointRouteBuilder app)
+    {
+        app.MapPost("/auth/forgot-password", async Task<EndpointResult<PasswordResetCompleted>>(
+            [FromBody] SendResetPasswordRequest request,
+            [FromServices] SendResetPasswordHandler handler,
+            CancellationToken cancellationToken) =>
+        {
+            return await handler.HandleAsync(request, cancellationToken);
+        });
+    }
+}
--- AuthService/AuthService.Web/Configuration/DependencyInjection.cs
+++ AuthService/AuthService.Web/Configuration/DependencyInjection.cs
@@ -5,6 +5,7 @@ using AuthService.Application.Features.ConfirmEmail;
 using AuthService.Application.Features.Login;
 using AuthService.Application.Features.RefreshToken;
 using AuthService.Application.Features.Register;
+using AuthService.Application.Features.SendPassResetLink;
 using AuthService.Core;
 using AuthService.Core.Database;
 using AuthService.Core.Identity;
@@ -51,10 +52,12 @@ public static class DependencyInjection
         services.AddScoped<ITransactionManager, TransactionManager>();
         services.AddScoped<ITokenProvider, TokenProvider>();
         services.AddScoped<IValidator<RegisterRequest>, RegisterValidator>();
+        services.AddScoped<IValidator<SendResetPasswordRequest>, SendResetPasswordValidator>();
         
         services.AddScoped<RefreshTokenHandler>();
         services.AddScoped<RegisterHandler>();
         services.AddScoped<LoginHandler>();
+        services.AddScoped<SendResetPasswordHandler>();
         services.AddScoped<IQueryHandler<ConfirmEmailQuery, UnitResult<Error>>, ConfirmEmailQueryHandler>();
         
         services.AddEndpoints(typeof(RegisterEndpoint).Assembly);
