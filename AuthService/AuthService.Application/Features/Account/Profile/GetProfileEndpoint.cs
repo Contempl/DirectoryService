6def8c3815517537224831a6cf91e5d0a345850b
contempl <the_mega_gamexc76@mail.ru>
2026-04-07 21:24:45 +0300

    User profile feature

---
+++ AuthService/AuthService.Application/Features/Account/Profile/GetProfileEndpoint.cs
@@ -0,0 +1,20 @@
+﻿using AuthService.Contracts.Dto;
+using Framework.Response;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+
+namespace AuthService.Application.Features.Account.Profile;
+
+public class GetProfileEndpoint : IEndpoint
+{
+    public void MapEndpoint(IEndpointRouteBuilder app)
+    {
+        app.MapGet("auth/me", async Task<EndpointResult<UserProfileDto>> (
+            [FromServices] GetProfileHandler handler, 
+            CancellationToken cancellationToken) =>
+        {
+            return await handler.HandleAsync(cancellationToken);
+        }).RequireAuthorization();
+    }
+}
