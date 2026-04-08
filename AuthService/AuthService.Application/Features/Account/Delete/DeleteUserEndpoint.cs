d4ec0e7d955aac182973a8d770feabdbed8e559f
contempl <the_mega_gamexc76@mail.ru>
2026-04-08 20:28:37 +0300

    Delete user

---
+++ AuthService/AuthService.Application/Features/Account/Delete/DeleteUserEndpoint.cs
@@ -0,0 +1,22 @@
+﻿using AuthService.Application.Extensions;
+using AuthService.Domain.Constants;
+using Framework.Response;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+
+namespace AuthService.Application.Features.Account.Delete;
+
+public class DeleteUserEndpoint : IEndpoint
+{
+    public void MapEndpoint(IEndpointRouteBuilder app)
+    {
+        app.MapPatch("users/{userId}/deactivate", async Task<EndpointResult> (
+            [FromRoute] Guid userId,
+            [FromServices] DeleteUserHandler handler,
+            CancellationToken cancellationToken) =>
+        {
+            return await handler.HandleAsync(userId, cancellationToken);
+        }).RequirePermissions(Permissions.USERS_MANAGE);
+    }
+}
---
+++ AuthService/AuthService.Application/Features/Account/Delete/DeleteUserHandler.cs
@@ -0,0 +1,76 @@
+﻿using AuthService.Application.Database;
+using AuthService.Domain.Entities;
+using CSharpFunctionalExtensions;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+using Shared.Kernel;
+
+namespace AuthService.Application.Features.Account.Delete;
+
+public class DeleteUserHandler
+{
+    private readonly IRefreshTokensRepository _refreshTokensRepository;
+    private readonly ITransactionManager _transactionManager;
+    private readonly UserManager<ApplicationUser> _userManager;
+    private readonly ILogger<DeleteUserHandler> _logger;
+
+    public DeleteUserHandler(
+        IRefreshTokensRepository refreshTokensRepository,
+        ITransactionManager transactionManager,
+        UserManager<ApplicationUser> userManager,
+        ILogger<DeleteUserHandler> logger)
+    {
+        _refreshTokensRepository = refreshTokensRepository;
+        _transactionManager = transactionManager;
+        _userManager = userManager;
+        _logger = logger;
+    }
+
+    public async Task<UnitResult<Error>> HandleAsync(Guid userId, CancellationToken cancellationToken)
+    {
+        var beginTransaction = await _transactionManager.BeginTransactionAsync(cancellationToken);
+        if (beginTransaction.IsFailure)
+        {
+            _logger.LogInformation("Failed to begin transaction.");
+            return beginTransaction.Error;
+        }
+
+        using var transactionScope = beginTransaction.Value;
+
+        try
+        {
+            var user = await _userManager.FindByIdAsync(userId.ToString());
+            if (user is null)
+            {
+                _logger.LogInformation("User not found.");
+                return GeneralErrors.NotFound(name: nameof(ApplicationUser));
+            }
+        
+            var deleteResult = user.Deactivate();
+            if (deleteResult.IsFailure)
+            {
+                _logger.LogInformation("Failed to deactivate user.");
+                return deleteResult.Error;
+            }
+        
+            await _refreshTokensRepository.RevokeAllRefreshTokensFromUser(userId, cancellationToken);
+
+            var commitResult = transactionScope.Commit();
+            if (commitResult.IsFailure)
+            {
+                _logger.LogInformation("Failed to commit transaction.");
+                return commitResult.Error;
+            }
+
+            return UnitResult.Success<Error>();
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError("Failure while removing user.");
+            
+            transactionScope.Rollback();
+            
+            return GeneralErrors.Failure();
+        }
+    }
+}
