8856d17ce8279aa588a62b597d66a100cba2e228
contempl <the_mega_gamexc76@mail.ru>
2026-04-08 20:28:53 +0300

    Assign role request

---
+++ AuthService/AuthService.Application/Features/Account/AddRoles/AssignRoleEndpoint.cs
@@ -0,0 +1,21 @@
+﻿using Framework.Response;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+
+namespace AuthService.Application.Features.Account.AddRoles;
+
+public class AssignRoleEndpoint : IEndpoint
+{
+    public void MapEndpoint(IEndpointRouteBuilder app)
+    {
+        app.MapPost("users/{userId}/roles", async Task<EndpointResult> (
+            [FromRoute] Guid userId,
+            [FromServices] AssignRoleHandler handler,
+            AssignRoleRequest request,
+            CancellationToken cancellationToken) =>
+        {
+            return await handler.HandleAsync(userId, request, cancellationToken);
+        });
+    }
+}
