0e124ecc1a42d2d4be3e2d5fc93529209d524e3f
contempl <the_mega_gamexc76@mail.ru>
2026-08-16 23:35:23 +0300

    feat: Кэширование presigned-ссылок в FileService через Redis

---
+++ FileService/FileService.Core/Caching/FileCacheKeys.cs
@@ -0,0 +1,13 @@
+﻿namespace FileService.Core.Caching;
+
+public static class FileCacheKeys
+{
+    private const string DownloadUrlPrefix = "file:download-url:";
+
+    public static readonly TimeSpan DownloadUrlExpiration = TimeSpan.FromMinutes(35);
+
+    public static readonly TimeSpan DownloadUrlLocalExpiration = TimeSpan.FromMinutes(5);
+
+    public static string DownloadUrl(Guid fileId) =>
+        $"{DownloadUrlPrefix}{fileId:D}";
+}
